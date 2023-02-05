bc9a1d7aebc2ddcb05d9961d64c6be1cc319f24e
Milos Roknic <roknic.milos.994@gmail.com>
2023-02-05 16:47:51 +0100

    Implement login page

--- app/src/src/App.test.tsx
+++ app/src/src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders title', () => {
+  render(<App/>);
+  const title = screen.getByText(/Welcome to the Meme Wars/i);
+  expect(title).toBeInTheDocument();
+});
+
+test('Renders google login button', () => {
+  render(<App/>);
+  const buttonText = screen.getByText(/Login with google/i);
+  expect(buttonText).toBeInTheDocument();
 });
--- app/src/src/App.tsx
+++ app/src/src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styles from './App.module.scss'
+
+import googleColoredLogo from './assets/google-colored.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome to the Meme Wars</h1>
+      <button className={styles.googleLoginButton}>
+        <img src={googleColoredLogo} alt="google-colored-logo"/>
+        <span className={styles.googleLoginButtonLabel}>Login with Google</span>
+      </button>
     </div>
   );
 }
