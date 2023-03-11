93d93505f94c85da2cf106d8c6657da182b6177c
Milos Roknic <roknic.milos.994@gmail.com>
2023-03-11 15:46:58 +0100

    Fail github build if less than 100% test coverage and romove the same restriction from check_project.sh

--- api/scripts/check_project.sh
+++ api/scripts/check_project.sh
@@ -5,7 +5,7 @@ set -e
 . /app/scripts/colored_print.sh
 
 printc "\nRunning tests in parallel and checking test coverage...\n" "info"
-pytest --cov --cov-fail-under=100 -n auto
+pytest --cov -n auto
 
 printc "\nGenerating html for previously checked test coverage...\n" "info"
 coverage html
