4ab9b896b117996f9a06ab1f30516beb4ba0c03e
Milos Roknic <roknic.milos.994@gmail.com>
2023-03-12 11:45:43 +0100

    APP | Implement sticky navbar

--- app/src/src/ui/App.tsx
+++ app/src/src/ui/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { router } from "../router";
 import { RouterProvider } from "react-router-dom";
 import { Navigation } from "./components/navigation/Navigation";
-
+import styles from "./main.module.scss";
 
 export function App() {
   return (
     <>
       <Navigation/>
-      <RouterProvider router={ router }/>
+      <div className={ styles.content }>
+        <RouterProvider router={ router }/>
+      </div>
     </>
   );
 }
