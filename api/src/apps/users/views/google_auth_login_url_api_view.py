ce01e44a4c4e950f9dce0a2a7ecf5401ec750f06
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-22 12:39:44 +0200

    API | Add Open API docs to GoogleAuthLoginUrlAPIView get method

--- api/src/apps/users/views/google_auth_login_url_api_view.py
+++ api/src/apps/users/views/google_auth_login_url_api_view.py
@@ -1,5 +1,7 @@
+from django.utils.translation import gettext_lazy as _
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
+from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework.views import APIView
 
 from apps.users.authentication import google_auth
@@ -7,6 +9,13 @@ from apps.users.authentication import google_auth
 
 class GoogleAuthLoginUrlAPIView(APIView):
 
+    @extend_schema(
+        responses={
+            302: OpenApiResponse(
+                description=_('Redirects to a Google login URL')
+            )
+        }
+    )
     def get(self, *args, **kwargs) -> HttpResponseRedirect:
         google_login_url = google_auth.get_login_url(request=self.request)
         return redirect(to=google_login_url)
