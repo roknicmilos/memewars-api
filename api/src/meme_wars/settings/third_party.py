03bc3d203cb0e7893a2fd25cfa39c09f50a99a58
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-23 17:10:26 +0200

    API | Add DRF_PAGE_SIZE env var

--- api/src/meme_wars/settings/third_party.py
+++ api/src/meme_wars/settings/third_party.py
@@ -20,7 +20,7 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'EXCEPTION_HANDLER': 'apps.common.utils.handle_api_exception',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': config('DRF_PAGE_SIZE', cast=int, default=10),
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
