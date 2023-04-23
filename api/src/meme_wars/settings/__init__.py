51835ca9bbeae01954e291f29d087a8486d728f6
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-23 17:01:20 +0200

    API | Order Memes by total score in MemeListCreateAPIView

--- api/src/meme_wars/settings/__init__.py
+++ api/src/meme_wars/settings/__init__.py
@@ -1,6 +1,6 @@
-from meme_wars.settings.core import *  # noqa: F403
-from meme_wars.settings.third_party import *  # noqa: F403
-from meme_wars.settings.custom import *  # noqa: F403
+from meme_wars.settings.core import *
+from meme_wars.settings.third_party import *
+from meme_wars.settings.custom import *
 
 
 INSTALLED_APPS += [
