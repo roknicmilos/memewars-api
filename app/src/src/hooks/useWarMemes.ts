63cbdcb89fe4b21a6280c530e1bcf9cdbfe22554
Milos Roknic <roknic.milos.994@gmail.com>
2023-04-23 22:46:09 +0200

    APP | Upgrade useWarMemes hook and use it in FinishedWar

--- app/src/src/hooks/useWarMemes.ts
+++ app/src/src/hooks/useWarMemes.ts
@@ -1,29 +1,41 @@
 import { Meme } from "../models/Meme";
-import { useEffect, useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { memeService } from "../services/memeService";
+import { useVisible } from "./useVisible";
+import { API } from "../services/apiClient";
 
 interface UseWarMemesReturnValue {
   memes: Meme[];
   setMemes(memes: Meme[]): void;
   isLoading: boolean;
   setIsLoading(isLoading: boolean): void;
+  memesBottomRef: MutableRefObject<null>;
+  nextPage?: number;
 }
 
 export function useWarMemes(warID: number): UseWarMemesReturnValue {
   const [ isLoading, setIsLoading ] = useState<boolean>(true);
   const [ memes, setMemes ] = useState<Meme[]>([]);
+  const [ nextPage, setNextPage ] = useState<number | undefined>(1);
+  const memesBottomRef = useRef(null);
+  const isSeeingEndOfList = useVisible(memesBottomRef);
 
   async function fetchMemes() {
-    const pageResponse = await memeService.getMemes(warID);
-    setMemes(pageResponse.results);
+    if (nextPage === undefined) {
+      throw Error("Unable to fetch the next page of memes because there is no next page");
+    }
+    const pageResponse = await memeService.getMemes(warID, nextPage);
+    setMemes([ ...memes, ...pageResponse.results ]);
+    const hasNextPage = pageResponse.count / API.PAGE_SIZE > nextPage;
+    setNextPage(hasNextPage ? nextPage + 1 : undefined);
     setIsLoading(false);
   }
 
   useEffect(() => {
-    if (isLoading) {
+    if (isLoading || (isSeeingEndOfList && nextPage)) {
       fetchMemes();
     }
-  }, []);
+  }, [ isSeeingEndOfList ]);
 
-  return { memes, setMemes, isLoading, setIsLoading };
+  return { memes, setMemes, isLoading, setIsLoading, memesBottomRef, nextPage };
 }
--- app/src/src/ui/pages/war-details/finished-war/FinishedWar.tsx
+++ app/src/src/ui/pages/war-details/finished-war/FinishedWar.tsx
@@ -1,41 +1,17 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { War } from "../../../../models/War";
-import { memeService } from "../../../../services/memeService";
 import styles from "./FinishedWar.module.scss";
 import { FinishedWarMeme } from "./meme/FinishedWarMeme";
 import { Loader } from "../../../components/loader/Loader";
 import { WarHeader } from "../../../components/war-header/WarHeader";
-import { Meme } from "../../../../models/Meme";
-import { useVisible } from "../../../../hooks/useVisible";
-import { API } from "../../../../services/apiClient";
+import { useWarMemes } from "../../../../hooks/useWarMemes";
 
 interface FinishedWarProps {
   war: War;
 }
 
 export function FinishedWar({ war }: FinishedWarProps) {
-  const [ isLoading, setIsLoading ] = useState<boolean>(true);
-  const [ memes, setMemes ] = useState<Meme[]>([]);
-  const [ nextPage, setNextPage ] = useState<number | undefined>(1);
-  const memesBottomRef = useRef(null);
-  const isSeeingEndOfList = useVisible(memesBottomRef);
-
-  async function fetchMemes() {
-    if (nextPage === undefined) {
-      throw Error("Unable to fetch the next page of memes because there is no next page");
-    }
-    const pageResponse = await memeService.getMemes(war.id, nextPage);
-    setMemes([ ...memes, ...pageResponse.results ]);
-    const hasNextPage = pageResponse.count / API.PAGE_SIZE > nextPage;
-    setNextPage(hasNextPage ? nextPage + 1 : undefined);
-    setIsLoading(false);
-  }
-
-  useEffect(() => {
-    if (isLoading || (isSeeingEndOfList && nextPage)) {
-      fetchMemes();
-    }
-  }, [ isSeeingEndOfList ]);
+  const { memes, isLoading, memesBottomRef, nextPage } = useWarMemes(war.id);
 
   if (isLoading) return <Loader/>;
 
