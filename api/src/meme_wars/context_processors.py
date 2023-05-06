0af6b30f2ed07a103af560ca738e906bcfffd8fa
Milos Roknic <roknic.milos.994@gmail.com>
2023-05-06 16:49:09 +0200

    Refactor env variables and make the project environment agnostic

--- api/src/meme_wars/context_processors.py
+++ api/src/meme_wars/context_processors.py
@@ -1,11 +1,8 @@
-from decouple import config
-
 from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
 
 
 def meme_wars(request: WSGIRequest) -> dict:
     return {
-        'environment': config('ENVIRONMENT'),
         'app_url': settings.CLIENT_APP_URL,
     }
--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -1,34 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-. /app/scripts/colored_print.sh
+. /app/scripts/utils.sh
 
 export REACT_APP_API_URL="$WEB_API_BASE_URL/api/v1"
 export REACT_APP_PAGE_SIZE=$DRF_PAGE_SIZE
 
-print_react_project_init_info() {
-  printc "Starting React app in $ENVIRONMENT mode \n" "info"
-  printc "WEB API URL: $REACT_APP_API_URL\n" "info"
-}
-
-init_react_project() {
-  if [ "$ENVIRONMENT" = 'development' ]; then
-    print_react_project_init_info
-    npm install
-    npm start
-
-  elif [ "$ENVIRONMENT" = 'production' ] || [ "$ENVIRONMENT" = 'staging' ]; then
-    print_react_project_init_info
-    npm install
-    npm run build
-    serve -s build
-
-  else
-    printc "[ERROR]: Unknown React app environment: '$ENVIRONMENT'\n" "danger"
-    printc "Exiting... \n\n"
-    exit 1
-  fi
-}
-
-init_react_project
+npm install
+if $(bool "$WEB_APP_DEV_SERVER"); then
+  printc "Starting development server...\n" "info"
+  npm start
+else
+  printc "Building production ready files and starting production server...\n" "info"
+  npm run build
+  serve -s build
+fi
