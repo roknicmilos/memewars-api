c53bb062f9e4158c67e60618972f86f6801c5157
Milos Roknic <roknic.milos.994@gmail.com>
2023-06-10 09:18:00 +0200

    Fix RedirectMiddlware

--- src/meme_wars/middlewares.py
+++ src/meme_wars/middlewares.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import redirect
 from django.urls import reverse
 
@@ -8,15 +9,29 @@ class RedirectMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        path = request.get_full_path()
+        if api_redirect_url := _get_api_redirect_url(request=request):
+            return redirect(to=api_redirect_url)
 
-        if path.startswith(reverse('api:index')) and settings.API_REDIRECT_URL:
-            return redirect(to=_create_redirect_url(base_url=settings.API_REDIRECT_URL, url_path=path))
-
-        if path.startswith(reverse('admin:index')) and settings.ADMIN_REDIRECT_URL:
-            return redirect(to=_create_redirect_url(base_url=settings.ADMIN_REDIRECT_URL, url_path=path))
+        if admin_redirect_url := _get_admin_redirect_url(request=request):
+            return redirect(to=admin_redirect_url)
 
         return self.get_response(request)
+
+
+def _get_api_redirect_url(request: WSGIRequest) -> str | None:
+    path = request.get_full_path()
+    absolute_uri = request.build_absolute_uri()
+    api_url = settings.API_REDIRECT_URL
+    if api_url and not absolute_uri.startswith(api_url) and path.startswith(reverse('api:index')):
+        return _create_redirect_url(base_url=api_url, url_path=path)
+
+
+def _get_admin_redirect_url(request: WSGIRequest) -> str | None:
+    path = request.get_full_path()
+    absolute_uri = request.build_absolute_uri()
+    admin_url = settings.ADMIN_REDIRECT_URL
+    if admin_url and not absolute_uri.startswith(admin_url) and path.startswith(reverse('admin:index')):
+        return _create_redirect_url(base_url=admin_url, url_path=path)
 
 
 def _create_redirect_url(base_url: str, url_path: str) -> str:
