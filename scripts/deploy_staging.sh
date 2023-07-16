cf29b494b02da3df8619c2205acafafd79c66364
Milos Roknic <roknic.milos.994@gmail.com>
2023-07-16 14:50:31 +0200

    Update deploy_staging.sh

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -9,9 +9,6 @@ echo "Current directory: $PWD"
 echo "Content of the current directory:"
 ls -la
 
-echo "Content of .env file:"
-cat .env
-
 docker compose build
 docker compose -p memewars-api-staging up -d
 docker exec -t memewars-django--staging sh -c 'pytest --create-db --cov -n auto && coverage html'
