98170cf80b29f58c23b71fc841ed26053ded50f3
Milos Roknic <roknic.milos.994@gmail.com>
2023-07-20 20:43:33 +0200

    Fix test.sh

--- scripts/pipeline/test.sh
+++ scripts/pipeline/test.sh
@@ -6,11 +6,11 @@ set -e
 # and the content of that location:
 echo "Current directory: $PWD"
 echo "Content of the current directory:"
-sh "ls -la"
+ls -la
 
 # Build and start the necessary containers that
 # will run tests, and clean them afterwards:
-sh "cp example.env .env"
-sh "docker compose -f docker-compose.test.yml build --no-cache"
-sh "docker compose -f docker-compose.test.yml run --rm django"
-sh "docker compose down"
+cp example.env .env
+docker compose -f docker-compose.test.yml build --no-cache
+docker compose -f docker-compose.test.yml run --rm django
+docker compose down
