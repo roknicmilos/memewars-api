7f375b8eed4f3a07cd2f651b0b78a6e586af5022
Milos Roknic <roknic.milos.994@gmail.com>
2023-07-29 19:12:09 +0200

    Update pipeline/test.sh

--- scripts/pipeline/test.sh
+++ scripts/pipeline/test.sh
@@ -8,9 +8,15 @@ echo "Current directory: $PWD"
 echo "Content of the current directory:"
 ls -la
 
-# Build and start the necessary containers that
-# will run tests, and clean them afterwards:
+# Build the necessary images with default environment variables:
 cp example.env .env
 docker compose -f docker-compose.test.yml build --no-cache
+
+# Make sure there are no containers from previous runs:
+docker compose -f docker-compose.test.yml down
+
+# Run "django" container that runs tests and checks:
 docker compose -f docker-compose.test.yml run --rm django
-docker compose down
+
+# Make sure to remove all started containers:
+docker compose -f docker-compose.test.yml down
